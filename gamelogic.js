0584153bb5299b9cde0b957ced5005d9f08f7abc
nateachino <natealleyne88@gmail.com>
2022-09-24 23:50:57 -0400

    Worked on game logic

---
+++ gamelogic.js
@@ -0,0 +1,77 @@
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3);
+  if (choice == 1) {
+    return "rock";
+  } else if (choice == 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function playerChoice() {
+  let playerChoice = prompt("Rock, Paper, or Scissors: ");
+  if (
+    playerChoice != "Rock" ||
+    playerChoice != "Paper" ||
+    playerChoice != "Scissors"
+  ) {
+  }
+}
+
+function playRound() {
+  let compChoice = getComputerChoice();
+  let playerChoice = prompt("Rock, Paper, or Scissors: ");
+  if (
+    playerChoice != "Rock" ||
+    playerChoice != "Paper" ||
+    playerChoice != "Scissors"
+  ) {
+    let playerChoice = prompt("Rock, Paper, or Scissors: ");
+  }
+  playerChoice = playerChoice.toLowerCase();
+  if (
+    playerChoice == "rock" ||
+    playerChoice == "paper" ||
+    playerChoice == "scissors"
+  ) {
+    if (playerChoice == compChoice) {
+      return 1;
+    } else if (
+      (playerChoice == "rock" && compChoice == "scissors") ||
+      (playerChoice == "paper" && compChoice == "rock") ||
+      (playerChoice == "scissors" && compChoice == "paper")
+    ) {
+      return 2;
+    } else if (
+      (playerChoice == "rock" && compChoice == "paper") ||
+      (playerChoice == "paper" && compChoice == "scissors") ||
+      (playerChoice == "scissors" && compChoice == "rock")
+    ) {
+      return 3;
+    }
+  }
+}
+
+function game() {
+  let compScore = 0;
+  let playerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    let winner = playRound();
+    if (winner == 1) {
+      return;
+    } else if (winner == 2) {
+      playerScore += 1;
+    } else if (winner == 3) {
+      compScore += 1;
+    }
+
+    if (compScore > playerScore) {
+      console.log("Computer Wins");
+    } else {
+      console.log("Player Wins");
+    }
+  }
+}
+
+game();
