7432c56ca0a11aa019bcc8cd3431c0228248a7a8
nateachino <natealleyne88@gmail.com>
2022-09-30 23:54:07 -0400

    finished project

--- gamelogic.js
+++ gamelogic.js
@@ -1,3 +1,9 @@
+let playerScore = 0;
+let compScore = 0;
+const playerScoreText = document.querySelector("#player");
+const computerScoreText = document.querySelector("#comp");
+const selected = document.querySelector("#selected");
+
 function getComputerChoice() {
   const choice = Math.floor(Math.random() * 3);
   if (choice == 1) {
@@ -9,61 +15,54 @@ function getComputerChoice() {
   }
 }
 
-function playRound() {
+function playRound(e) {
   let compChoice = getComputerChoice();
-  let playerChoice = prompt("Rock, Paper, or Scissors: ");
-  playerChoice = playerChoice.toLowerCase();
-
+  let playerChoice = e.target.id;
   if (
     playerChoice == "rock" ||
     playerChoice == "paper" ||
     playerChoice == "scissors"
   ) {
+    selected.textContent =
+      "Computer chose " + compChoice + "! You chose " + playerChoice;
     if (playerChoice === compChoice) {
-      return 1;
+      selected.append("! Draw!");
     } else if (
       (playerChoice == "rock" && compChoice == "scissors") ||
       (playerChoice == "paper" && compChoice == "rock") ||
       (playerChoice == "scissors" && compChoice == "paper")
     ) {
-      return 2;
+      playerScore += 1;
+      playerScoreText.textContent = "Player Score: " + playerScore;
+      selected.append("! Player Wins!");
     } else if (
       (playerChoice == "rock" && compChoice == "paper") ||
       (playerChoice == "paper" && compChoice == "scissors") ||
       (playerChoice == "scissors" && compChoice == "rock")
     ) {
-      return 3;
-    }
-  } else {
-    alert("Invalid input");
-    playRound();
-  }
-}
-
-function game() {
-  let compScore = 0;
-  let playerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    let winner = playRound();
-    if (winner == 1) {
-      playerScore += 0;
-      compScore += 0;
-    } else if (winner == 2) {
-      playerScore += 1;
-    } else if (winner == 3) {
       compScore += 1;
+      computerScoreText.textContent = "Computer Score: " + compScore;
+      selected.append("! Computer Wins!");
     }
   }
-
-  console.log("Player Score: " + playerScore);
-  console.log("Computer Score: " + compScore);
-  if (compScore > playerScore) {
-    console.log("Computer Wins");
-  } else if (playerScore > compScore) {
-    console.log("Player Wins");
-  } else if (playerScore == compScore) {
-    console.log("Tie");
+  if (playerScore == 5) {
+    selected.append(" Player wins the round");
+    compScore = 0;
+    playerScore = 0;
+    playerScoreText.textContent = "Player Score: " + playerScore;
+    computerScoreText.textContent = "Computer Score: " + compScore;
+  } else if (compScore == 5) {
+    selected.append(" Computer wins the round");
+    compScore = 0;
+    playerScore = 0;
+    playerScoreText.textContent = "Player Score: " + playerScore;
+    computerScoreText.textContent = "Computer Score: " + compScore;
   }
 }
 
-game();
+const buttons = document.querySelectorAll("button");
+const scoreDiv = document.querySelector(".score");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", playRound);
+});
